131521c007b6fa1e75d2229892f19bcde8a605d5
Marcel Huber <marcelhuberfoo@gmail.com>
2015-11-11 23:53:21 +0100

    removed interactive option from bash commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 umask 002
 
 if [ "$1" = 'dummy' ]; then
-  gosu $UNAME bash -li -c 'cd $HOME/coast && \
+  gosu $UNAME bash -l -c 'cd $HOME/coast && \
         scons -u --jobs=2 --with-src-boost=3rdparty/boost --ignore-missing --doxygen-only && \
         scons -u --jobs=2 CoastRecipes --with-src-boost=3rdparty/boost && \
 	cd apps/CoastRecipes && ln -s ../../doc/Coast/html COASTDoc'
   # -> stop this server using: docker kill --signal=TERM container
-  exec gosu $UNAME bash -li -c 'cd $HOME/coast && scons -u --jobs=2 CoastRecipes --with-src-boost=3rdparty/boost --run'
+  exec gosu $UNAME bash -l -c 'cd $HOME/coast && scons -u --jobs=2 CoastRecipes --with-src-boost=3rdparty/boost --run'
 fi
 exec "$@"
